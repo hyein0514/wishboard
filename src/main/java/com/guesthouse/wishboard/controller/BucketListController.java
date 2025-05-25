a88b11d1e2b42f123d66cfe449f7e312dea80dc6
geunyeong <rmsdud7763@gmail.com>
2025-05-26 00:32:25 +0900

    feat: 버킷리스트 등록 기능 구현 완료

---
+++ src/main/java/com/guesthouse/wishboard/controller/BucketListController.java
@@ -0,0 +1,22 @@
+package com.guesthouse.wishboard.controller;
+
+import com.guesthouse.wishboard.dto.BucketListRequestDto;
+import com.guesthouse.wishboard.service.BucketListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/bucketlist")
+@RequiredArgsConstructor
+public class BucketListController {
+
+    private final BucketListService bucketListService;
+
+    @PostMapping
+    public ResponseEntity<String> create(@RequestBody BucketListRequestDto dto) {
+        bucketListService.createBucketList(dto);
+        return ResponseEntity.ok("버킷리스트가 등록되었습니다.");
+    }
+}
+

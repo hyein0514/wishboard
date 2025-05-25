ea047dae99b2c557711f460a9233a19b57731d36
geunyeong <rmsdud7763@gmail.com>
2025-05-26 01:49:52 +0900

    feat: 도전 기록 목록 조회 API 구현

---
+++ src/main/java/com/guesthouse/wishboard/dto/BucketListLogResponseDto.java
@@ -0,0 +1,14 @@
+package com.guesthouse.wishboard.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class BucketListLogResponseDto {
+    private Long logId;
+    private String createdAt;
+    private String image;
+    private String content;
+}
+
--- src/main/java/com/guesthouse/wishboard/repository/BucketListLogRepository.java
+++ src/main/java/com/guesthouse/wishboard/repository/BucketListLogRepository.java
@@ -3,5 +3,8 @@ package com.guesthouse.wishboard.repository;
 import com.guesthouse.wishboard.entity.BucketList_log;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BucketListLogRepository extends JpaRepository<BucketList_log, Long> {
+    List<BucketList_log> findByBucketIdOrderByCreatedAtAsc(Long bucketId);
 }

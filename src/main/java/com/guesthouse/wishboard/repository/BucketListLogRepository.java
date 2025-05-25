5038323f099dd17bc4ecde18d0a339764dc9a959
geunyeong <rmsdud7763@gmail.com>
2025-05-26 02:12:57 +0900

    feat: 버킷리스트 도전기록 이전/다음 탐색 API 구현

--- src/main/java/com/guesthouse/wishboard/repository/BucketListLogRepository.java
+++ src/main/java/com/guesthouse/wishboard/repository/BucketListLogRepository.java
@@ -3,8 +3,11 @@ package com.guesthouse.wishboard.repository;
 import com.guesthouse.wishboard.entity.BucketList_log;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface BucketListLogRepository extends JpaRepository<BucketList_log, Long> {
     List<BucketList_log> findByBucketIdOrderByCreatedAtAsc(Long bucketId);
+    BucketList_log findTopByBucketIdAndCreatedAtLessThanOrderByCreatedAtDesc(Long bucketId, Date createdAt);
+    BucketList_log findTopByBucketIdAndCreatedAtGreaterThanOrderByCreatedAtAsc(Long bucketId, Date createdAt);
 }

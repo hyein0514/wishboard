4e0f612af6478887f51cd9381e1cc80246a17c1e
hyen <hi020514@gmail.com>
2025-06-01 14:37:25 +0900

    "fix: 충돌 수정"

--- src/main/java/com/guesthouse/wishboard/config/WebConfig.java
+++ src/main/java/com/guesthouse/wishboard/config/WebConfig.java
@@ -1,4 +1,3 @@
-// config/WebConfig.java
 package com.guesthouse.wishboard.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -9,27 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-            .allowedOriginPatterns("*")
-            .allowedOrigins("http://localhost:8081") // 또는 "*" (개발용)
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
-            .allowedHeaders("*")
-            .allowCredentials(true);
-  }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:8081") // 또는 "*" (개발용)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/uploaded/**")
-            // 실제 업로드된 파일이 저장된 로컬 경로
-            .addResourceLocations("file:C:/upload/")
-            .setCachePeriod(3600);
-  }
-            .allowedOrigins("http://localhost:8081") // 또는 "*" (개발용)
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
-  }
-
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploaded/**")
+                .addResourceLocations("file:C:/upload/")
+                .setCachePeriod(3600);
+    }
 }
